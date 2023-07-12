5e7002c05db332b45437df8919b538e65fe76f3f
Ewelina Musińska <ewelina.musinska@gmail.com>
2023-07-12 20:09:26 +0200

    Merge remote-tracking branch 'origin/development' into development

--- app/src/main/java/com/example/paytrack/LoginActivity.java
+++ app/src/main/java/com/example/paytrack/LoginActivity.java
@@ -46,7 +46,13 @@ public class LoginActivity extends AppCompatActivity {
                                     @Override
                                     public void onSuccess(AuthResult authResult) {
                                         Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
                                         finish();
+                                    }
+                                }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                                     }
                                 });
                     } else {
